dd3344966cf8eab8a159acd8bed238d871b05530
abhisharma1010 <141005817+abhisharma1010@users.noreply.github.com>
2025-12-04 21:53:45 +0530

    Added insert-data.sql

---
+++ sql-queries/insert-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO Students VALUES (1, 'Alice', 20, 'A');
+INSERT INTO Students VALUES (2, 'Bob', 21, 'B');
+
+INSERT INTO Courses VALUES (101, 'Java Basics');
+INSERT INTO Courses VALUES (102, 'SQL Fundamentals');
+
+INSERT INTO Enrollments VALUES (1, 1, 101);
+INSERT INTO Enrollments VALUES (2, 2, 102);
