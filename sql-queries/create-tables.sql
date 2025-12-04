29281f0b77ca382e0d633dd5809c94d17dadfc92
abhisharma1010 <141005817+abhisharma1010@users.noreply.github.com>
2025-12-04 21:52:55 +0530

    Added create-tables.sql

---
+++ sql-queries/create-tables.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Age INT,
+    Grade VARCHAR(10)
+);
+
+CREATE TABLE Courses (
+    CourseID INT PRIMARY KEY,
+    CourseName VARCHAR(50)
+);
+
+CREATE TABLE Enrollments (
+    EnrollmentID INT PRIMARY KEY,
+    StudentID INT,
+    CourseID INT,
+    FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
+    FOREIGN KEY (CourseID) REFERENCES Courses(CourseID)
+);
